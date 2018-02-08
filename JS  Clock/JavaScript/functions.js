22f5532365bce2aff0f3e5ea9706bcb4d21cf365
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-02-08 19:14:59 -0200

    JavaScript and CSS clock finished

---
+++ JS  Clock/JavaScript/functions.js
@@ -0,0 +1,22 @@
+
+const secHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const secs = now.getSeconds();
+    const secsDegrees = ((secs / 60) * 360) + 90;
+    secHand.style.transform = `rotate(${secsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
